2d77007023b9bb443a9beb5e39b4d40691738a98
Vikash Sinha <vikashsinha484@gmail.com>
2026-02-23 21:12:49 +0530

    JS Assignment Uploaded

---
+++ Day_4_JS/Assignment5.js
@@ -0,0 +1,14 @@
+let number = 7;
+
+let result = (number >= 0) ? "Positive" : "Negative";
+console.log(result);
+
+if (number % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+}
+
+for (let i = 1; i <= number; i++) {
+    console.log(i);
+}
